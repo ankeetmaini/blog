3717408ee7e2870c58b0466fd8ec6250244af7bc
Ankeet Maini <ankeet.maini@gmail.com>
2019-01-18 13:08:47 +0530

    Adds initial code for the blog

---
+++ src/components/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { rhythm } from '../utils/typography'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer
+        style={{
+          marginTop: rhythm(2.5),
+          paddingTop: rhythm(1),
+        }}
+      >
+        <a
+          href="https://mobile.twitter.com/ankeetmaini"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          twitter
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://github.com/ankeetmaini"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>{' '}
+        &bull;{' '}
+        <a
+          href="https://instagram.com/ankeetmaini"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          instagram
+        </a>
+      </footer>
+    )
+  }
+}
+
+export default Footer
