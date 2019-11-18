deedba4551be7cb1777521db62df5dc6aec70248
Ankeet Maini <ankeet.maini@gmail.com>
2019-11-19 00:18:00 +0530

    Move posts to the root

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog`,
+        path: `${__dirname}/posts`,
         name: `blog`,
       },
     },
