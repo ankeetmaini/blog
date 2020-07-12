56724e2d20007065feac0e0529813c60dd9e9e68
Ankeet Maini <ankeet.maini@gmail.com>
2020-07-12 13:54:27 +0530

    add home footer

---
+++ src/gatsby-theme-blog/components/home-footer.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react"
+import { Styled, css } from "theme-ui"
+
+const Footer = ({ socialLinks }) => (
+  <footer
+    css={css({
+      mt: 4,
+      pt: 3,
+    })}
+  >
+    © {new Date().getFullYear()}, Powered by
+    {` `}
+    lots of procrastination and frequent visits to the fridge
+    {` `}&bull;{` `}
+    {socialLinks
+      ? socialLinks.map((platform, i, arr) => (
+          <Fragment key={platform.url}>
+            <Styled.a
+              href={platform.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {platform.name}
+            </Styled.a>
+            {arr.length - 1 !== i && (
+              <Fragment>
+                {` `}&bull;{` `}
+              </Fragment>
+            )}
+          </Fragment>
+        ))
+      : null}
+  </footer>
+)
+export default Footer
