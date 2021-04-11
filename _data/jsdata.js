0ee141f0fce6909cdc32f2f69fed8d78315c5df2
Ankeet Maini <ankeet.maini@gmail.com>
2021-04-11 14:16:22 +0530

    add speaking stuff

--- _data/jsdata.js
+++ _data/jsdata.js
@@ -8,5 +8,13 @@ module.exports = {
       date: "24 Oct 2020",
       at: "Fullstack Javascript Earth",
     },
+    {
+      title: "Amplify with Friends - Hackathon Winners",
+      deck:
+        "https://under-engineered.hashnode.dev/hobbithole-your-personal-habit-tracker-with-friends",
+      video: "https://www.twitch.tv/videos/944284482",
+      date: "10 Mar 2021",
+      at: "AWS Twitch Stream",
+    },
   ],
 };
