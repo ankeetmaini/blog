7423f0be1ea7ef3c7946bbaccc7316b8d877009d
Ankeet Maini <ankeet.maini@gmail.com>
2021-04-11 14:23:04 +0530

    Backend (#22)
    * Backend
    
    * some more
    
    * finish
    
    * fix css
    
    * Add space
    
    * fix links
    
    * add speaking stuff
    
    * speaking
    
    Co-authored-by: Ankeet Maini <ankeetmaini@Anils-MacBook-Air.local>

--- _data/jsdata.js
+++ _data/jsdata.js
@@ -8,5 +8,19 @@ module.exports = {
       date: "24 Oct 2020",
       at: "Fullstack Javascript Earth",
     },
+    {
+      title: "Amplify with Friends - Hackathon Winners",
+      deck:
+        "https://under-engineered.hashnode.dev/hobbithole-your-personal-habit-tracker-with-friends",
+      video: "https://www.twitch.tv/videos/944284482",
+      date: "10 Mar 2021",
+      at: "AWS Twitch Stream",
+    },
+    {
+      title: "React Day - Web Performance Panel Discussion",
+      video: "https://www.youtube.com/watch?v=76simEsemlY",
+      date: "19 Mar 2021",
+      at: "React Day Conference",
+    },
   ],
 };
