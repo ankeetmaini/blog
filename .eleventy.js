4729123817888cde0338a0f982cae6e13e9d13d6
Ankeet Maini <ankeet.maini@gmail.com>
2021-06-26 23:30:26 +0530

    remove #

--- .eleventy.js
+++ .eleventy.js
@@ -79,7 +79,6 @@ module.exports = function (eleventyConfig) {
   }).use(markdownItAnchor, {
     permalink: true,
     permalinkClass: "direct-link",
-    permalinkSymbol: "#",
   });
   eleventyConfig.setLibrary("md", markdownLibrary);
 
