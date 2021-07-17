07466df40a020f9eb96d8ed6daff461d366772d9
Ankeet Maini <ankeet.maini@gmail.com>
2021-07-17 19:59:52 +0530

    Grpc web (#37)
    * grpc-web
    
    * done
    
    * now done
    
    * typo
    
    * heading
    
    * add more dets
    
    * modify a line
    
    * add description

--- .eleventy.js
+++ .eleventy.js
@@ -61,6 +61,9 @@ module.exports = function (eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("jpg");
+  eleventyConfig.addPassthroughCopy("jpeg");
+  eleventyConfig.addPassthroughCopy("png");
   eleventyConfig.addPassthroughCopy("css");
 
   /* Markdown Overrides */
