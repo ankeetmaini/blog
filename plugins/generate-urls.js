97f2b29cd021407579feb2d29430981fd21422bb
Ankeet Maini <ankeet.maini@gmail.com>
2021-07-19 00:00:40 +0530

    tet

--- plugins/generate-urls.js
+++ plugins/generate-urls.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const parser = require('fast-xml-parser');
 
 const netlifySlug = '${{ steps.netlify.outputs.url }}/';
+const netlifySlug1 = 'adfdf';
 const prodHostname = 'https://ankeetmaini.dev/';
 
 const siteMap = fs.readFileSync('./_site/sitemap.xml').toString();
@@ -12,6 +13,6 @@ const dom = parser.parse(siteMap);
 
 const urls = dom.urlset.url
     .filter(u => u && u.loc && !u.loc.includes('/tags/'))
-    .map(u => u.loc.replace(prodHostname, netlifySlug));
+    .map(u => u.loc.replace(prodHostname, netlifySlug1));
 
 console.log(urls);
