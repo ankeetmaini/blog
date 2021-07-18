b3b28e314616787fdf184f7aa498c11e3ecc5b68
Ankeet Maini <ankeet.maini@gmail.com>
2021-07-18 23:57:13 +0530

    light house server

---
+++ plugins/generate-urls.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const parser = require('fast-xml-parser');
+
+const netlifySlug = '${{ steps.netlify.outputs.url }}/';
+const prodHostname = 'https://ankeetmaini.dev/';
+
+const siteMap = fs.readFileSync('./_site/sitemap.xml').toString();
+
+const dom = parser.parse(siteMap);
+
+const urls = dom.urlset.url
+    .filter(u => u && u.loc && !u.loc.includes('/tags/'))
+    .map(u => u.loc.replace(prodHostname, netlifySlug));
+
+console.log(urls);
