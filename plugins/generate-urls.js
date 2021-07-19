e74ae80ec0425cea7e5978ef7a7dbfc8920916b3
Ankeet Maini <ankeet.maini@gmail.com>
2021-07-19 10:09:07 +0530

    add urls to config instead of env vars

--- plugins/generate-urls.js
+++ plugins/generate-urls.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const parser = require('fast-xml-parser');
 
 const netlifySlug = '${{ steps.netlify.outputs.url }}/';
-const netlifySlug1 = 'adfdf';
 const prodHostname = 'https://ankeetmaini.dev/';
 
 const siteMap = fs.readFileSync('./_site/sitemap.xml').toString();
@@ -13,6 +12,7 @@ const dom = parser.parse(siteMap);
 
 const urls = dom.urlset.url
     .filter(u => u && u.loc && !u.loc.includes('/tags/'))
-    .map(u => u.loc.replace(prodHostname, netlifySlug1));
+    .map(u => u.loc.replace(prodHostname, netlifySlug));
 
-console.log(urls);
+const config = { urls };
+fs.writeFileSync('./.github/workflows/lighthouserc.json', JSON.stringify(config, null, 2));
