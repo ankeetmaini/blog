0a0da8b40b4d05218db016d1d34f379e86cb6efa
Ankeet Maini <ankeet.maini@hotstar.com>
2024-03-09 15:54:14 +0530

    try 1

--- eleventy.config.js
+++ eleventy.config.js
@@ -42,12 +42,12 @@ module.exports = function (eleventyConfig) {
     // Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
     return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(
       format || "dd LLLL yyyy"
-    );
+    ).toLowerCase();
   });
 
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd").toLowerCase();
   });
 
   // Get the first `n` elements of a collection.
